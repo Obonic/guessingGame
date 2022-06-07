f60d450edc045829d5f128d0355db0b6173be07f
Obonic <55562725+Obonic@users.noreply.github.com>
2022-06-07 13:56:48 -0700

    Add files via upload
    guess what number the app is thinking of you get 5 tries.

---
+++ guessingGame/Program.cs
@@ -0,0 +1,16 @@
+﻿
+var randonum = new Random().Next(1, 10);
+//Console.WriteLine("Secret Number is:" + randonum);  //Tells the user the secret number
+for (var i = 0; i < 5; i++)
+{
+    Console.WriteLine("Please Enter in a Number from 1 - 10");
+    var guess = Convert.ToInt32(Console.ReadLine());
+
+    if (guess == randonum)
+    {
+        Console.WriteLine("You Correct! The secret number was: " + randonum + "!!!");
+        return;
+    }
+}
+Console.WriteLine("You failed to correctly guess the secret number. :(");
+Console.WriteLine("The secret Number was: " + randonum);
